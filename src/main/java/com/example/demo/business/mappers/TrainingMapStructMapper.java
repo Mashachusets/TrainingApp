5c02bb6c909828a609d29547d41f002b6e58997c
kaklauskaite.marija <mkmk.kaklauskaite@gmail.com>
2023-01-16 12:20:07 +0200

    Changed dependency versions Added mapstruct dependency Changed annotations

--- src/main/java/com/example/demo/business/mappers/TrainingMapStructMapper.java
+++ src/main/java/com/example/demo/business/mappers/TrainingMapStructMapper.java
@@ -2,9 +2,9 @@ package com.example.demo.business.mappers;
 
 import com.example.demo.business.repository.model.TrainingDAO;
 import com.example.demo.model.Training;
-import org.apache.ibatis.annotations.Mapper;
+import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface TrainingMapStructMapper {
 
     TrainingDAO mapToDAO(Training training);
--- src/main/java/com/example/demo/business/repository/model/TrainingDAO.java
+++ src/main/java/com/example/demo/business/repository/model/TrainingDAO.java
@@ -1,14 +1,10 @@
 package com.example.demo.business.repository.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
